01f05d6cec711a5210d9199109507a666e8cdb1d
Như <truonghuynhnhu1412@gmail.com>
2022-01-18 17:42:55 +0700

    first commit

---
+++ Test Suites/Test suites/Regression/Folder 1/Folder 1.1/Test Suite 1.1.1.1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1.1.1.1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>30d5befa-5f84-4ba4-aa91-7c0a290a5039</testSuiteGuid>
+   <testCaseLink>
+      <guid>bceecbb4-9f32-435c-bf13-35fb86acbf8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folder 3/Folder 3.1/Folder 3.1.1/welcome not have maintainer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9d2f03fb-bce3-4c87-9038-9e9bc51eabbf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folder 3/Folder 3.2/カードはホームページに表示されます</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Test suites/Regression/Folder 1/Folder 1.1/Test Suite Regression.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c6c6ddd2-5df3-452b-9a01-1d1fc0b8a078</testSuiteGuid>
+   <testCaseLink>
+      <guid>9196cc5b-ef98-4103-97fc-f3fd7a48a8ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folder 1/Shown logo - hiển thị logo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>14624c49-6ecd-4d00-afc6-79fb62980305</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folder 2/显示的徽标 - hiển thị 徽标</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
