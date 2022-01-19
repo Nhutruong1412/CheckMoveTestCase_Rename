22430cec65dd0f9828a1a43ae218b87294eb792e
Như <truonghuynhnhu1412@gmail.com>
2022-01-19 16:06:57 +0700

    first commit

---
+++ Test Suites/Test suites/Unit test rename/Đây là một test suite rất là dài luôn dài lắm tin tui đi nó dài đến nổi.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Đây là một test suite rất là dài luôn dài lắm tin tui đi nó dài đến nổi</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f3190921-6c09-4860-94af-67d088aa8bff</testSuiteGuid>
+   <testCaseLink>
+      <guid>191e5c35-cf86-4a70-be2a-6a3b14df98cf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folder 1/Shown logo - hiển thị logo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
